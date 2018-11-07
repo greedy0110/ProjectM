c095008eb55c68d9a81a4b46f7f61e1474408b86
greedy <greedy0110@gmail.com>
2018-11-07 15:11:27 +0900

    전화오면 잠금화면 안만들도록 변경
     - 사용자 권한 추가 "android.permission.READ_PHONE_STATE"

--- app/src/main/java/com/example/a/lockquizekotlin/LockScreen/UnlockReceiver.kt
+++ app/src/main/java/com/example/a/lockquizekotlin/LockScreen/UnlockReceiver.kt
@@ -3,21 +3,44 @@ package com.example.a.lockquizekotlin.LockScreen
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.telephony.TelephonyManager
 import android.util.Log
 import com.example.a.lockquizekotlin.DBContract.SettingsContract
+import android.telephony.PhoneStateListener
+
+
 
 class UnlockReceiver : BroadcastReceiver() {
     val TAG = "UnlockReceiver"
+    private var mTelephonyManager: TelephonyManager? = null
+    private val phoneListener = object : PhoneStateListener() {
+        override fun onCallStateChanged(state: Int, incomingNumber: String) {
+            when (state) {
+                TelephonyManager.CALL_STATE_IDLE -> mIsPhoneIdle = true
+                TelephonyManager.CALL_STATE_RINGING -> mIsPhoneIdle = false
+                TelephonyManager.CALL_STATE_OFFHOOK -> mIsPhoneIdle = false
+            }
+        }
+    }
+    private var mIsPhoneIdle = true
 
     override fun onReceive(context: Context, intent: Intent) {
         Log.d(TAG, "onReceive")
         if (intent.action == Intent.ACTION_SCREEN_ON) {
-            // LockScreenService 를 시작해주어야 한다.
-            // 이미 LockScreenService 를 띄워줄 권한은 받은 상태여야 한다.
-            val intent = Intent(context, LockScreenService::class.java)
-            val setting = SettingsContract.getSettingsEntry(context)
-            intent.putExtra("forceLockPeriod", setting.slideForcePeriod)
-            context.startService(intent)
+            // 전화 통화가 온 상태면 키면 화가 난다.
+            if (mTelephonyManager == null) {
+                mTelephonyManager = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+            }
+            mTelephonyManager?.listen(phoneListener, PhoneStateListener.LISTEN_CALL_STATE)
+
+            if (mIsPhoneIdle) {
+                // LockScreenService 를 시작해주어야 한다.
+                // 이미 LockScreenService 를 띄워줄 권한은 받은 상태여야 한다.
+                val intent = Intent(context, LockScreenService::class.java)
+                val setting = SettingsContract.getSettingsEntry(context)
+                intent.putExtra("forceLockPeriod", setting.slideForcePeriod)
+                context.startService(intent)
+            }
         }
     }
 }
